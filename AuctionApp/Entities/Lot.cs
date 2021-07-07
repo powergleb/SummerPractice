cc1ab3d589fc773c07cc64c22cd6d787efb7c1f6
g.rokah@yandex.ru <g.rokah@yandex.ru>
2021-07-07 17:57:33 +0300

    edited project structure, added enetities

---
+++ AuctionApp/Entities/Lot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+    class Lot
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public int StrtingPrice { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public int? BuyingUserId { get; set; }
+        public int SellingUserId { get; set; }
+
+    }
+}
---
+++ AuctionApp/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+    class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+        public string Login { get; set; }
+        public string Pass { get; set; }
+        public int Age { get; set; }
+    }
+
+
+   
+}
